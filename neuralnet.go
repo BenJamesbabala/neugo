54e8a5225f87df946ce91a389a381a874f94b7b0
jinseokYeom <jinyeom95@gmail.com>
2016-08-09 03:09:24 -0500

    progress

--- neuralnet.go
+++ neuralnet.go
@@ -4,3 +4,16 @@ type NeuralNet struct {
 	conf    *Config   // configuration
 	weights []*Matrix // list of weights
 }
+
+func NewNeuralNet(conf *Config) *NeuralNet {
+	return &NeuralNet{
+		conf: conf,
+		weights: func() []*Matrix {
+			weights := make([]*Matrix)
+
+			// to be implemented
+
+			return weights
+		}(),
+	}
+}
