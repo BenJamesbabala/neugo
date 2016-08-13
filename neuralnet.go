77d36d22f0cb3396bd2ee120c64e3f67f7266b5e
jinseokYeom <jinyeom95@gmail.com>
2016-08-13 10:49:49 -0400

    progress

--- neuralnet.go
+++ neuralnet.go
@@ -1,6 +1,10 @@
 package neugo
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/jinseokYeom/neugo/matrix"
+)
 
 var (
 	// wrong number of inputs
@@ -8,25 +12,25 @@ var (
 )
 
 type NeuralNet struct {
-	conf    *Config   // configuration
-	weights []*Matrix // list of weights
+	conf    *Config          // configuration
+	weights []*matrix.Matrix // list of weights
 }
 
 func NewNeuralNet(conf *Config) *NeuralNet {
 	return &NeuralNet{
 		conf: conf,
-		weights: func() []*Matrix {
-			weights := make([]*Matrix, 2)
+		weights: func() []*matrix.Matrix {
+			weights := make([]*matrix.Matrix, 2)
 			// input layer -> hidden layer
-			weights[0] = NewNorm(conf.NumInput,
+			weights[0] = matrix.NewNorm(conf.NumInput,
 				conf.NumHidden, 0.0, 6.0)
 			// hidden layer[t] -> hidden layer[t+1]
 			for i := 1; i < conf.NumLayer-1; i++ {
-				weights[i] = NewNorm(conf.NumHidden,
+				weights[i] = matrix.NewNorm(conf.NumHidden,
 					conf.NumHidden, 0.0, 6.0)
 			}
 			// hidden layer -> output layer
-			weights[conf.NumLayer-1] = NewNorm(conf.NumHidden,
+			weights[conf.NumLayer-1] = matrix.NewNorm(conf.NumHidden,
 				conf.NumOutput, 0.0, 6.0)
 			return weights
 		}(),
@@ -34,7 +38,7 @@ func NewNeuralNet(conf *Config) *NeuralNet {
 }
 
 // Get the neural network's weights.
-func (n *NeuralNet) Weights() []*Matrix {
+func (n *NeuralNet) Weights() []*matrix.Matrix {
 	return weights
 }
 
