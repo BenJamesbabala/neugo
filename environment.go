1e7a225a3bcc28bd568dae65ac80ec6b660d1aaf
jinseokYeom <jinyeom95@gmail.com>
2016-08-13 16:06:47 -0400

    initial commit

---
+++ environment.go
@@ -0,0 +1,7 @@
+package neugo
+
+// Environment is defined as a function that takes a
+// neural network and put it to test. It can either perform
+// reinforcement learning and return the fitness, or perform
+// a supervised learning and return the error.
+type Environment func(*NeuralNet) float64
