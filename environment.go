5fe6feee5a34afe07a3aaae73d28b107f31c21b6
jinseokYeom <jinyeom95@gmail.com>
2016-08-16 02:06:05 -0500

    added xor problem for testing

--- environment.go
+++ environment.go
@@ -1,7 +1,30 @@
 package neugo
 
+import "math"
+
 // Environment is defined as a function that takes a
 // neural network and puts it to test. It can either perform
 // reinforcement learning and return the fitness, or perform
 // a supervised learning and return the error.
 type Environment func(*NeuralNet) float64
+
+// Test environment 1: XOR
+// In this environment, a neural network is tested on
+// XOR problems: the neural net is tested on 0 xor 0,
+// 0 xor 1, 1 xor 0, and 1 xor 1; this environment returns
+// total error for all four tests.
+func XORTest() Environment {
+	return func(nn *NeuralNet) float64 {
+		totalErr := 0.0
+		// 0 xor 0
+		output, _ := nn.Feedforward([]float64{0.0, 0.0})
+		totalErr += math.Pow(output[0]-0.0, 2.0)
+		output, _ = nn.Feedforward([]float64{0.0, 1.0})
+		totalErr += math.Pow(output[0]-1.0, 2.0)
+		output, _ = nn.Feedforward([]float64{1.0, 0.0})
+		totalErr += math.Pow(output[0]-1.0, 2.0)
+		output, _ = nn.Feedforward([]float64{1.0, 1.0})
+		totalErr += math.Pow(output[0]-0.0, 2.0)
+		return totalErr
+	}
+}
